4e78e739333e187e70641dc2d533f0d365576c11
Rafa Gómez <rgomezcasas@gmail.com>
2024-08-05 18:17:51 +0200

    ci: extract tasks to Makefile

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ release-minor:
 
 release-major:
 	@GIT_EDITOR='echo "Release" >' vendor/bin/monorepo-builder release major
+
+lint:
+	@./vendor/bin/ecs check
+
+tests:
+	@./vendor/bin/phpunit packages/*/tests
--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,12 +3,9 @@
 declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
@@ -21,8 +18,5 @@ return static function (MBConfig $mbConfig): void {
 		SetCurrentMutualDependenciesReleaseWorker::class,
 		TagVersionReleaseWorker::class,
 		PushTagReleaseWorker::class,
-		SetNextMutualDependenciesReleaseWorker::class,
-		UpdateBranchAliasReleaseWorker::class,
-		PushNextDevReleaseWorker::class,
 	]);
 };
