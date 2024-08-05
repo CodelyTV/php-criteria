310200415dc9f226bdb79ba667b18d5c82d760ae
Rafa Gómez <rgomezcasas@gmail.com>
2024-08-05 22:18:26 +0200

    feat: transform likes to eloquent query

--- packages/criteria-to-eloquent/src/CriteriaToEloquentConverter.php
+++ packages/criteria-to-eloquent/src/CriteriaToEloquentConverter.php
@@ -5,14 +5,28 @@ declare(strict_types=1);
 namespace CodelyTv\Criteria\Eloquent;
 
 use CodelyTv\Criteria\Criteria;
+use CodelyTv\Criteria\FilterOperator;
 use Illuminate\Database\Eloquent\Builder;
 
 final readonly class CriteriaToEloquentConverter
 {
+	private const OPERATORS = [
+		FilterOperator::EQUAL->value => '=',
+		FilterOperator::NOT_EQUAL->value => '!=',
+		FilterOperator::GT->value => '>',
+		FilterOperator::LT->value => '<',
+		FilterOperator::CONTAINS->value => 'like',
+		FilterOperator::NOT_CONTAINS->value => 'not like',
+	];
+
 	public function convert(Builder $queryBuilder, Criteria $criteria): Builder
 	{
 		foreach ($criteria->filters()->filters() as $filter) {
-			$queryBuilder->where($filter->field()->value(), $filter->operator()->value, $filter->value()->value());
+			$operator = self::OPERATORS[$filter->operator()->value];
+
+			$value = $filter->operator()->isContaining() ? "%{$filter->value()->value()}%" : $filter->value()->value();
+
+			$queryBuilder->where($filter->field()->value(), $operator, $value);
 		}
 
 		if ($criteria->hasOrder()) {
